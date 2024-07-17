796d26db08a65f4759194831e1d74cd831c92804
HADJADJ DAOUD <daoudhdj@gmail.com>
2024-07-17 14:58:56 +0100

    complet delete user-list

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -136,6 +136,23 @@ export default function Profile() {
       showListingsError(true);
     }
   };
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="max-w-lg p-3 mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7 ">Profile</h1>
@@ -251,7 +268,12 @@ export default function Profile() {
                 </p>
               </Link>
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase ">delete</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase "
+                >
+                  delete
+                </button>
                 <button className="text-green-700 uppercase ">edit</button>
               </div>
             </div>
